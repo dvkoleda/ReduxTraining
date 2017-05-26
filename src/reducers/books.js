21400c80d01d2cd5886c20b950cba42d97be616c
koleda_d <deniskoleda@iba.by>
2017-05-26 11:49:32 +0300

    Added book list with state

---
+++ src/reducers/books.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Koleda_D on 25.05.2017.
+ */
+
+export default function() {
+    return [
+        {title: "Bla 1"},
+        {title: "bla 2"},
+        {title: "bla 3"},
+    ]
+}
